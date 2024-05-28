bb0b73beaf7392e78b64c1a68b333fd637e6736b
Daniela Carballo <daniela.carballo@biggertech.co>
2024-05-27 23:37:56 -0300

    Add validation to files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -6,14 +7,22 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from 'src/files/helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file)
+      throw new BadRequestException('Make sure that the file is an image');
+
     return {
       fileName: file.originalname,
     };
