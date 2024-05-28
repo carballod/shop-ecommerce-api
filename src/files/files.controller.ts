14b2e4c231d8aec5378642ccb473162df27f4a68
Daniela Carballo <daniela.carballo@biggertech.co>
2024-05-27 23:18:23 -0300

    Add module to update files

---
+++ src/files/files.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      fileName: file.originalname,
+    };
+  }
+}
