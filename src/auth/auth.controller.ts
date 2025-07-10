cd4fd37311807854a40413465fbf8e03d125962d
Daniela Carballo <danscarballo@gmail.com>
2025-07-09 23:10:11 -0300

    Implement role-based access control with UserRoleGuard and RoleProtected decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from 'src/auth/dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { GetUser, RawHeaders, RoleProtected } from 'src/auth/decorators';
 import { User } from 'src/auth/entities/user.entity';
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from 'src/auth/guards/user-role.guard';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -19,15 +30,36 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-  @Get()
+  @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
-    // @Req() request: Express.Request,
-    @GetUser(['email', 'role', 'fullName']) user: User,
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
   ) {
+    // console.log(request);
+
     return {
       ok: true,
       message: 'You are authenticated',
+      user,
+      userEmail,
+      rawHeaders,
+      headers,
+    };
+  }
+
+  // @SetMetadata('roles', ['admin', 'super-user']) -> RoleProtected decorator
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(@GetUser() user: User) {
+    // console.log(request);
+
+    return {
+      ok: true,
       user,
     };
   }
--- src/auth/decorators/index.ts
+++ src/auth/decorators/index.ts
@@ -1,2 +1,4 @@
 export * from './get-user.decorator';
+export * from './index';
 export * from './raw-headers.decorator';
+export * from './role-protected.decorator';
