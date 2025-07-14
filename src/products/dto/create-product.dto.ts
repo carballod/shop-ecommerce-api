fce31808ff55fadff944ec4e6c34ce7ec084338b
Daniela Carballo <danscarballo@gmail.com>
2025-07-14 00:20:14 -0300

    Enhance CreateProductDto with additional ApiProperty decorators for tags and images;

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,84 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: 'Product price',
+    nullable: false,
+    type: Number,
+    minimum: 0,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    description: 'Product description',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: 'Product SLUG - for SEO',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    description: 'Product stock',
+    nullable: false,
+    type: Number,
+    minimum: 0,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    description: 'Product sizes',
+    nullable: false,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product gender',
+    nullable: false,
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    description: 'Product tags',
+    nullable: true,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags?: string[];
 
+  @ApiProperty({
+    description: 'Product images',
+    nullable: true,
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
